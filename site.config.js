4b51516ab3fec2d3d1eceb452b2ea6d2cced6f4e
not_lazy_bear <28949342+lgw26046044@users.noreply.github.com>
2021-08-04 15:17:58 +0800

    修改个人信息

--- site.config.js
+++ site.config.js
@@ -1,25 +1,26 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '8c6d8027188247e2b72b0bea9bb6dd8b',
+
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '不懒熊-树屋',
+  domain: 'https://torch-wallflower-96a.notion.site/HomePage-8c6d8027188247e2b72b0bea9bb6dd8b',
+  author: '不懒熊',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: '该站点用于记录自己的个人计划、心得经验及资料的整理',
+  socialImageTitle: '不懒熊',
+  socialImageSubtitle: '欢迎👋,来看不懒熊🐻, 这里是日常生活记录, 包括: 个人计划、心得经验、资料整理等',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'not_lazy_bear',
+  github: 'lgw26046044',
+  linkedin: 'not_lazy_bear',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
